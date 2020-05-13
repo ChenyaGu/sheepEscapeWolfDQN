627f99ac7bb45f168d520e3dfe4d45a0e3dbaf97
ChenyaGu <me_chenya@126.com>
2020-05-13 11:25:30 +0800

    update test

--- test/testMDP.py
+++ test/testMDP.py
@@ -8,37 +8,56 @@ class TestMDP(unittest.TestCase):
     def setUp(self):
         self.sheepId = 0
         self.wolfId = 1
-        self.stateSize = stateSize
-        self.actionSize = actionSize
-        self.model = model
+        self.movingRange = [0, 0, 15, 15]
+        self.speedList = [8, 9, 10]
+        self.currentVelocity = [0, 0]
+        # self.stateSize = stateSize
+        self.actionSize = 16
+        # self.model = model
         self.epsilon = 0.05
 
-    @data(([0, 0], [1, 1], 2 ** 0.5),
-          ([1, -1], [-1, 1], 8 ** 0.5))
+    # @data(([0, 0], [1, 1], 2 ** 0.5),
+    #       ([1, -1], [-1, 1], 8 ** 0.5))
+    # @unpack
+    # def testSelectAction(self, p1, p2, groundTruthDis):
+    #     # pass?
+    #    transition = Transition(self.movingRange, self.speedList, self.actionSize)
+    #    newState = transition(currentState, currentAction, currentVelocity, identity)
+    #    truthValue = np.array_equal(newState, groundTruthState)
+    #    self.assertTrue(truthValue)
+
+    @data(([10, 10], 0, [12, 10]),
+          ([0, 0], 8, [8, 0]))
     @unpack
-    def testSelectAction(self, p1, p2, groundTruthDis):
-        # pass?
-        initialPosition = InitialPosition(self.movingRange, self.minDistanceEachOther, self.maxDistanceEachOther,
-                                          self.minDistanceWolfSheep)
-        positionList = initialPosition(self.numberObjects)
+    def testTransition(self, currentState, currentAction, groundTruthState):
+        # pass
+        currentVelocity = self.currentVelocity
+        identity = self.sheepId
+        transition = Transition(self.movingRange, self.speedList, self.actionSize)
+        newState = transition(currentState, currentAction, currentVelocity, identity)
+        truthValue = np.array_equal(newState, groundTruthState)
+        self.assertTrue(truthValue)
 
-
-    @data(([0, 0], [1, 1], 2 ** 0.5),
-          ([1, -1], [-1, 1], 8 ** 0.5))
+    @data((([0, 0], [0, 1], [1, 1]), ([1, 50], [0, 11]), 1, -100),
+          (([0, 0], [0, 2], [0, 1]), ([1, 50], [0, 11]), 1, 111))
     @unpack
-    def testTransition(self, p1, p2, groundTruthDis):
-        # pass?
+    def testReward(self, state, belief, minDis, groundTruthValue):
+        # pass
+        reward = Reward(self.sheepId, self.wolfId)
+        rewardValue = reward(state, belief, minDis)
+        truthValue = np.array_equal(rewardValue, groundTruthValue)
+        self.assertTrue(truthValue)
 
-
-    @data(([0, 0], [1, 1], 2 ** 0.5),
-          ([1, -1], [-1, 1], 8 ** 0.5))
+    @data((([0, 0], [1, 1]), 1, 0),
+          (([0, 0], [0, 1]), 1, 1))
     @unpack
-    def testReward(self, p1, p2, groundTruthDis):
-        # pass?
+    def testIsEnd(self, state, minDis, groundTruthValue):
+        # pass
+        isEnd = IsEnd(state, self.sheepId, self.wolfId)
+        value = isEnd(minDis)
+        truthValue = np.array_equal(value, groundTruthValue)
+        self.assertTrue(truthValue)
 
 
-    @data((([0, 0], [1, 1]), 2 ** 0.5),
-         (([0, 0], [1, 1]), 8 ** 0.5))
-    @unpack
-    def testIsEnd(self, p1, p2, groundTruthDis):
-        # pass?
+if __name__ == '__main__':
+    unittest.main()
